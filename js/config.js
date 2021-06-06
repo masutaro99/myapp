6382a9f809bdcf4cbbb96f32fd133dd7fa281386
masutaro99 <maskenpa1001@gmail.com>
2021-06-07 00:32:42 +0900

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-northeast-1_BbyqPgevk", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "24h1fkk9k2ncltgcfc8g4rg766", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
